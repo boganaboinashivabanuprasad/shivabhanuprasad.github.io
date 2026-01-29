8d4f7781d9f12351a431fb963f16da12b9fd717c
Shiva Banu Prasad Boganaboina <boganaboinashivabanuprasad@gmail.com>
2026-01-29 23:20:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Smooth scroll for navbar links
+const navLinks = document.querySelectorAll('nav ul li a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', function(e){
+    e.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    const targetSection = document.getElementById(targetId);
+    window.scrollTo({
+      top: targetSection.offsetTop - 60,
+      behavior: 'smooth'
+    });
+  });
+});
